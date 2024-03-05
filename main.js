44d2353e6c1f29af163b97bc0a316e15e3ada142
Syed-Faizan-Mustafa <faizan.mustafa30@gmail.com>
2024-03-05 19:05:16 +0500

    100 days challenge, 9 Questions Solutions

---
+++ main.js
@@ -0,0 +1,30 @@
+// Q1 is Installation and installations are already done.
+// Q2:
+// let personName: string = "Faizan Mustafa";
+// console.log(`Hello`, personName, "would you like to learn some typescript today.")
+// Q3:
+let personName = "Syed Faizan Mustafa";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log("faizan mustafa\nFAIZAN MUSTAFA\nFaizan Mustafa");
+// Q4
+console.log("Audrey hypburn once said!", '"Word IMPOSSIBLE itself says I AM POSSIBLE"');
+// Q5
+let famous_person = "Audrey Hypburn";
+let message = 'once said! "Word IMPOSSIBLE itself says I AM POSSIBLE"';
+console.log(famous_person, message);
+// Q6
+let nament = "\n\tSyed Faizan Mustafa\t\n";
+console.log(nament);
+let removent = nament.trim();
+console.log(removent);
+// Q7 & Q8
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+//Q9
+let favouritenumber = 10;
+console.log("My favourite number is", favouritenumber);
+export {};
---
+++ main.ts
@@ -0,0 +1,38 @@
+// Q1 is Installation and installations are already done.
+
+// Q2:
+// let personName: string = "Faizan Mustafa";
+// console.log(`Hello`, personName, "would you like to learn some typescript today.")
+
+// Q3:
+let personName: string = "Syed Faizan Mustafa";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+
+console.log("faizan mustafa\nFAIZAN MUSTAFA\nFaizan Mustafa")
+
+// Q4
+console.log("Audrey hypburn once said!", '"Word IMPOSSIBLE itself says I AM POSSIBLE"');
+
+// Q5
+let famous_person: string = "Audrey Hypburn";
+let message: string = 'once said! "Word IMPOSSIBLE itself says I AM POSSIBLE"';
+console.log(famous_person, message);
+
+// Q6
+let nament: string = "\n\tSyed Faizan Mustafa\t\n";
+console.log(nament);
+let removent = nament.trim();
+console.log(removent);
+
+// Q7 & Q8
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(16 / 2);
+
+//Q9
+let favouritenumber: number = 10
+console.log("My favourite number is", favouritenumber);
+
